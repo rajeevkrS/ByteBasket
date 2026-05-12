65e71ef86dcb727ce54910deafdc3957d10ffd0e
Rajeev Kumar Sudhansu <rajeevkumarr1221@gmail.com>
2026-05-12 15:50:14 +0530

    Successfully created User Role schema

---
+++ server/src/models/User.ts
@@ -0,0 +1,84 @@
+import mongoose from "mongoose";
+
+export type UserRole = "user" | "admin";
+
+// Address Schema
+const addressSchema = new mongoose.Schema(
+  {
+    fullName: {
+      type: String,
+      required: true,
+      trim: true,
+    },
+
+    address: {
+      type: String,
+      required: true,
+      trim: true,
+    },
+
+    state: {
+      type: String,
+      required: true,
+      trim: true,
+    },
+
+    postalCode: {
+      type: String,
+      required: true,
+      trim: true,
+    },
+
+    isDefault: {
+      type: true,
+      default: false,
+    },
+  },
+  {
+    timestamps: false,
+  },
+);
+
+// User Schema
+const UserSchema = new mongoose.Schema(
+  {
+    clerkUserId: {
+      type: String,
+      required: true,
+      unique: true,
+      index: true,
+    },
+
+    name: {
+      type: String,
+      required: false,
+    },
+
+    email: {
+      type: String,
+      required: false,
+    },
+
+    role: {
+      type: String,
+      enum: ["user", "admin"],
+      default: "user",
+    },
+
+    points: {
+      type: Number,
+      default: 0,
+      min: 0,
+    },
+
+    addresses: {
+      type: [addressSchema],
+      default: [],
+    },
+  },
+  {
+    timestamps: true,
+  },
+);
+
+export const User = mongoose.models.User || mongoose.model("User", UserSchema);
